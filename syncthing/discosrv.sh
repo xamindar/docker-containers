c1505e20893c044a8df4f717a15350e07d1648e0
xamindar <xamindar@gmail.com>
2016-01-15 23:14:56 -0800

    supervisor corrections

--- syncthing/discosrv.sh
+++ syncthing/discosrv.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #start discosrv using environment variables
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!!!!!!!!!!!!!!! STARTING DISCOVERY SERVER !!!!!!!!!!!!!!!!"
+echo "use instructions: http://docs.syncthing.net/users/discosrv.html" 
 /opt/discosrv/discosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" -stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
 
--- syncthing/relaysrv.sh
+++ syncthing/relaysrv.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #launch relaysrv with env variables
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!!!!!!!!!!!!!!!!! STARTING RELAY SERVER !!!!!!!!!!!!!!!!!!"
+echo "use instrustions: http://docs.syncthing.net/users/relaying.html"
 /opt/relaysrv/relaysrv -keys="$SYNCTHING_CONFIG/relaysrv" -pools="$RELAY_POOL" -global-rate="$RELAY_GLOBAL_RATE" -per-session-rate="$RELAY_SESSION_RATE" -message-timeout="$RELAY_MESSAGE_TIMEOUT" -network-timeout="$RELAY_NETWORK_TIMEOUT" -ping-interval="$RELAY_PING_INTERVAL" -ext-address="$RELAY_EXT_ADDRESS"
 
