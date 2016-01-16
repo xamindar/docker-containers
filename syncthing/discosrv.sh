035588449f52a83c2d58d79449a6fb304bef2a21
xamindar <xamindar@gmail.com>
2016-01-15 22:25:36 -0800

    supervisor corrections

--- syncthing/discosrv.sh
+++ syncthing/discosrv.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #start discosrv using environment variables
 /opt/discosrv/discosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" -stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
 
--- syncthing/relaysrv.sh
+++ syncthing/relaysrv.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #launch relaysrv with env variables
 /opt/relaysrv/relaysrv -keys="$SYNCTHING_CONFIG/relaysrv" -pools="$RELAY_POOL" -global-rate="$RELAY_GLOBAL_RATE" -per-session-rate="$RELAY_SESSION_RATE" -message-timeout="$RELAY_MESSAGE_TIMEOUT" -network-timeout="$RELAY_NETWORK_TIMEOUT" -ping-interval="$RELAY_PING_INTERVAL" -ext-address="$RELAY_EXT_ADDRESS"
 
