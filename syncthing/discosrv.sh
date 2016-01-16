8a07972035eab035c22aea2fc2df6755e2d2b402
xamindar <xamindar@gmail.com>
2016-01-15 21:14:20 -0800

    supervisor corrections

---
+++ syncthing/discosrv.sh
@@ -0,0 +1,3 @@
+#start discosrv using environment variables
+/opt/discosrv/discosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" -stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
+
