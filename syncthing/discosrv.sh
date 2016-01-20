6cd195207eb0943e7cf5e7759c2f73eefb99d1e7
xamindar <xamindar@gmail.com>
2016-01-20 09:47:40 -0800

    supervisor corrections

--- syncthing/discosrv.sh
+++ syncthing/discosrv.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #start discosrv using environment variables
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "!!!!!!!!!!!!!!!! STARTING DISCOVERY SERVER !!!!!!!!!!!!!!!!"
-echo "use instructions: http://docs.syncthing.net/users/discosrv.html" 
-/opt/discosrv/discosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" -stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
+echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!! STARTING DISCOVERY SERVER !!!!!!!!!!!!!!!!\nuse instructions: http://docs.syncthing.net/users/discosrv.html\nModify the following Discovery server URL accordingly and add it to all of your syncthing clients:\nhttps://[unraid machine address]:8443/?id=[Server device ID from next line]"
+/opt/discosrv/discosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" #-stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
 echo "!!!!!!!!! Discovery service died! Ensure config variables are set correctly. !!!!!!!!!!"
