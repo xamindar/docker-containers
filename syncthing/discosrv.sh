674ba68d26c60b62e6c1a18054c91750ac3e2d06
xamindar <xamindar@gmail.com>
2016-05-19 10:24:30 -0700

    Syncthing docker update for v0.13.x

--- syncthing/discosrv.sh
+++ syncthing/discosrv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #start discosrv using environment variables
-echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!! STARTING DISCOVERY SERVER !!!!!!!!!!!!!!!!\nuse instructions: http://docs.syncthing.net/users/discosrv.html\nModify the following Discovery server URL accordingly and add it to all of your syncthing clients:\nhttps://[unraid machine address]:8443/v2?id=[Server device ID from next line]"
-/opt/discosrv/discosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" -stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
+echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!! STARTING DISCOVERY SERVER !!!!!!!!!!!!!!!!\nuse instructions: http://docs.syncthing.net/users/discosrv.html\nModify the following Discovery server URL accordingly and add it to all of your syncthing clients:\nhttps://[unraid machine address]/v2/?id=[Server device ID from next line]"
+/opt/discosrv/discosrv -cert="$SYNCTHING_CONFIG/discosrv/cert.pem" -key="$SYNCTHING_CONFIG/discosrv/key.pem" #-stats-file="$SYNCTHING_CONFIG/discosrv/stats.log"
 echo "!!!!!!!!! Discovery service died! Ensure config variables are set correctly. !!!!!!!!!!"
