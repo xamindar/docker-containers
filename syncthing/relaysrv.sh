c7e0157075900190854e78c8ac8b723bbd596016
xamindar <xamindar@gmail.com>
2016-07-06 08:24:38 -0700

    Corrected URL links in logs

--- syncthing/relaysrv.sh
+++ syncthing/relaysrv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #launch relaysrv with env variables
-echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!!!! STARTING RELAY SERVER !!!!!!!!!!!!!!!!!!\nuse instrustions: http://docs.syncthing.net/users/relaying.html"
+echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n!!!!!!!!!!!!!!!!!! STARTING RELAY SERVER !!!!!!!!!!!!!!!!!!\nuse instrustions: https://docs.syncthing.net/users/strelaysrv.html"
 /opt/relaysrv/relaysrv -keys="$SYNCTHING_CONFIG/relaysrv" -pools="$RELAY_POOL" -global-rate="$RELAY_GLOBAL_RATE" -per-session-rate="$RELAY_SESSION_RATE" -message-timeout="$RELAY_MESSAGE_TIMEOUT" -network-timeout="$RELAY_NETWORK_TIMEOUT" -ping-interval="$RELAY_PING_INTERVAL"
 echo "!!!!!!!!! Relay service died! Ensure config variables are set correctly. !!!!!!!!!!"
